f42273dfdd8c08de6c550557100dd1778a743b9e
Satoshi Sahara <sahara.satoshi@gmail.com>
2017-07-13 08:53:29 +0900

    add action.php in deleted.files

--- action.php
+++ action.php
@@ -13,14 +13,14 @@ class action_plugin_pagetitle extends DokuWiki_Action_Plugin {
     /**
      * register the event handlers
      */
-    public function register(Doku_Event_Handler $controller) {
+    function register(Doku_Event_Handler $controller) {
         $controller->register_hook('DOKUWIKI_STARTED', 'BEFORE', $this, 'deleteObsoletedSingleClass');
     }
 
     /**
      * Delete syntax.php which is obsoleted since multi-components syntax structure
      */
-    public function deleteObsoletedSingleClass(Doku_Event $event) {
+    function deleteObsoletedSingleClass(Doku_Event $event) {
         $legacyFile = dirname(__FILE__).'/syntax.php';
         if (file_exists($legacyFile)) { unlink($legacyFile); }
     }
